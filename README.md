1594e90ef99ea4c73d7246e06fa8de33ce4cdeca
James Porter <james@amimetic.co.uk>
2026-01-10 21:21:42 +0000

    last major pieces for first version

--- README.md
+++ README.md
@@ -7,7 +7,12 @@ Copy and paste [preluade.js](./prelude.js) into the code editor for some common 
 ## Plan
 
 - [x] RNG stuff
-- [ ] Vector Stuff (2D and 3D)
-- [ ] Poisson Disc
-- [ ] Hexagon and triangular grid helpers
+- [x] Vector Stuff (2D and 3D)
+- [x] Poisson Disc
+- [x] Basic helpers as suggested by replicad docs
+- [ ] Testing
+
+## Future/maybe
+
 - [ ] (Perlin) Noise(?)
+- [ ] Hexagon and triangular grid helpers
--- prelude.js
+++ prelude.js
@@ -1,3 +1,26 @@
+/**
+ * Replicad Prelude
+ */
+
+function fuseAll(shapes) {
+  let result = shapes[0];
+  shapes.slice(1).forEach((shape) => {
+    result = result.fuse(shape);
+  });
+  return result;
+}
+
+function polarCopies(shape, count, radius) {
+  const base = shape.translate(0, radius);
+  const angle = 360 / count;
+
+  const copies = [];
+  for (let i = 0; i < count; i++) {
+    copies.push(base.clone().rotate(i * angle));
+  }
+  return copies;
+}
+
 class RNG {
   /**
    * Creates a new seeded random number generator.
